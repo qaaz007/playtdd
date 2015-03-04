6b3da07c2745f9bf2fcf09bab5330d012c91b4c0
Adnan <adnan.asia@gmail.com>
2015-03-03 16:17:23 -0800

    basic test committ

---
+++ src/play/tdd/com/_SavingsAccountTest.java
@@ -0,0 +1,14 @@
+package play.tdd.com;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class _SavingsAccountTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
